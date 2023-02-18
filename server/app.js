663694995e34b429c86c6c6614fe22e324e13f1d
princekr96 <princekr700@gmail.com>
2023-02-18 13:44:01 +0530

    mongodb config added

--- server/app.js
+++ server/app.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.set('strictQuery', false);
+
+// connect to mongodb db
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+      'mongodb+srv://thekrprince:thekrprince@gql-learning.6nkdtyb.mongodb.net/?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+      }
+    );
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+connectDB();
 
 app.use(
   '/graphql',
