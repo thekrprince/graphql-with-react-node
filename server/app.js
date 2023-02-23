4965bd20208c68d3c3c49cf3361fa874e4d9c21b
princekr96 <princekr700@gmail.com>
2023-02-23 15:56:13 +0530

    added cors in server

--- server/app.js
+++ server/app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
 mongoose.set('strictQuery', false);
+
+// allow cross-origin requests
+app.use(cors());
 
 // connect to mongodb db
 const connectDB = async () => {
